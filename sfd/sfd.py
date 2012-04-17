1edf5eef728a7d4ece4c9f0a782b5dc3f52e3f6a
Fantomas42 <fantomas42@gmail.com>
2012-04-17 14:58:55 +0200

    cleanup and apply my patches

--- sfd/sfd.py
+++ sfd/sfd.py
@@ -1,11 +1,11 @@
 """
-nose plugin for easy testing of django projects and apps. Sets up a test
+Nose plugin for easy testing of django projects and apps. Sets up a test
 database (or schema) and installs apps from test settings file before tests
 are run, and tears the test database (or schema) down after all tests are run.
 """
-
-import os, sys
+import os
 import re
+import sys
 
 from nose.plugins import Plugin
 import nose.case
@@ -13,10 +13,11 @@ import nose.case
 # Force settings.py pointer
 # search the current working directory and all parent directories to find
 # the settings file
-from nose.importer import add_path
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-import re
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 NT_ROOT = re.compile(r"^[a-zA-Z]:\\$")
+
+
 def get_SETTINGS_PATH():
     '''
     Hunt down the settings.py module by going up the FS path
@@ -35,7 +36,7 @@ def get_SETTINGS_PATH():
 SETTINGS_PATH = get_SETTINGS_PATH()
 
 
-class NoseDjango(Plugin):
+class SimpleFastDjango(Plugin):
     """
     Enable to set up django test environment before running all tests, and
     tear it down after all tests. If the django database engine in use is not
@@ -47,7 +48,7 @@ class NoseDjango(Plugin):
     into sys.path if it isn't already there, unless the -P
     (--no-path-adjustment) argument is set.
     """
-    name = 'django'
+    name = 'sfd'
 
     def configure(self, options, conf):
         Plugin.configure(self, options, conf)
@@ -58,44 +59,33 @@ class NoseDjango(Plugin):
         connection over to that database. Then call install() to install
         all apps listed in the loaded settings module.
         """
-        # Add the working directory (and any package parents) to sys.path
-        # before trying to import django modules; otherwise, they won't be
-        # able to find project.settings if the working dir is project/ or
-        # project/..
-
-        if not SETTINGS_PATH:
-            sys.stderr.write("Can't find Django settings file!\n")
-            # short circuit if no settings file can be found
-            return
-
-        if self.conf.addPaths:
-            map(add_path, self.conf.where)
-
-        add_path(SETTINGS_PATH)
-        sys.path.append(SETTINGS_PATH)
-        import settings
+        # use dynamical import: we not necessary use settings.py
+        __import__(os.environ['DJANGO_SETTINGS_MODULE'])
+        settings = sys.modules[os.environ['DJANGO_SETTINGS_MODULE']]
 
         # Some Django code paths evaluate differently
         # between DEBUG and not DEBUG.  Example of this include the url
         # dispatcher when 404's are hit.  Django's own test runner forces DEBUG
         # to be off.
-        settings.DEBUG = False 
+        settings.DEBUG = False
 
         from django.core import mail
         self.mail = mail
         from django.conf import settings
-        from django.core import management
         from django.test.utils import setup_test_environment
         from django.db import connection
 
-        self.old_db = settings.DATABASE_NAME
+        try:
+            self.old_db = settings.DATABASE_NAME
+        except AttributeError:
+            self.old_db = settings.DATABASES['default']['NAME']
 
         # setup the test env for each test case
         setup_test_environment()
         connection.creation.create_test_db(verbosity=self.verbosity)
 
         # exit the setup phase and let nose do it's thing
-            
+
     def beforeTest(self, test):
 
         if not SETTINGS_PATH:
@@ -104,7 +94,7 @@ class NoseDjango(Plugin):
 
         # This is a distinctive difference between the NoseDjango
         # test runner compared to the plain Django test runner.
-        # Django uses the standard unittest framework and resets the 
+        # Django uses the standard unittest framework and resets the
         # database between each test *suite*.  That usually resolves
         # into a test module.
         #
@@ -122,7 +112,8 @@ class NoseDjango(Plugin):
             hasattr(test.context, 'fixtures'):
                 # We have to use this slightly awkward syntax due to the fact
                 # that we're using *args and **kwargs together.
-                call_command('loaddata', *test.context.fixtures, **{'verbosity': 0}) 
+                call_command('loaddata', *test.context.fixtures,
+                             **{'verbosity': 0})
 
         if isinstance(test, nose.case.Test) and \
             isinstance(test.test, nose.case.MethodTestCase) and \
@@ -146,10 +137,11 @@ class NoseDjango(Plugin):
         from django.test.utils import teardown_test_environment
         from django.db import connection
         from django.conf import settings
-        connection.creation.destroy_test_db(self.old_db, verbosity=self.verbosity)   
+        from django.core.urlresolvers import clear_url_caches
+        connection.creation.destroy_test_db(
+            self.old_db, verbosity=self.verbosity)
         teardown_test_environment()
 
         if hasattr(self, 'old_urlconf'):
             settings.ROOT_URLCONF = self.old_urlconf
             clear_url_caches()
-
