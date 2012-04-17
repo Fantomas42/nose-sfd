511e7f1790c018d4c2d2fd066dc7fa0a752a8200
Fantomas42 <fantomas42@gmail.com>
2012-04-17 12:03:12 +0200

    updating setup script

--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
-from setuptools import setup, find_packages
+"""Setup script for nose-sfd"""
+from setuptools import setup
+from setuptools import find_packages
+
+import sfd
 
 setup(
-    name='NoseDjango',
-    version='0.6',
-    author='Victor Ng',
-    author_email = 'crankycoder@gmail.com',
-    description = 'nose plugin for easy testing of django projects ' \
-        'and apps. Sets up a test database (or schema) and installs apps ' \
-        'from test settings file before tests are run, and tears the test ' \
-        'database (or schema) down after all tests are run.',
+    name='nose-sfd',
+    version=sfd.__version__,
+
+    description='nose plugin for testing Django projects in a faster way.',
+    long_description=open('README.rst').read(),
+    keywords='nose, django, tests',
+
+    author=sfd.__author__,
+    author_email=sfd.__email__,
+    url=sfd.__url__,
+
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'],
+    license=sfd.__license__,
+    include_package_data=True,
+
     install_requires='nose>=0.10',
-    url = "http://www.assembla.com/spaces/nosedjango",
-    license = 'GNU LGPL',
-    packages = find_packages(),
-    zip_safe = False,
-    include_package_data = True,
-    entry_points = {
+    entry_points={
         'nose.plugins': [
-            'django = nosedjango.nosedjango:NoseDjango',
+            'django = sfd.sfd:SimpleFastDjango',
             ]
         }
     )
-
--- sfd/__init__.py
+++ sfd/__init__.py
@@ -1,1 +1,8 @@
 """Nose Simple and Fast Django module"""
+__version__ = '0.1.dev'
+__license__ = 'GNU LGPL'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'https://github.com/Fantomas42/nose-sfd'
